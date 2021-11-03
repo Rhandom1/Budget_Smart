40422846cb8ddb6aeb631934697fee567989a26d
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-11-03 09:54:45 -0500

    Created manifest and initial service worker file

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    "/manifest.webmanifest",
+    // Add cache files here
+];
+
+// Add event listener for install 19.2.12
+    // skipwaiting
+// Add event listner for activate
+// Add event for fetch
+// Add event for respondWith
